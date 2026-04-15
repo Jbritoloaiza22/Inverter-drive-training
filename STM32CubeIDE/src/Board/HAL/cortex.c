f07850c86e972121c6ff50bcaa169fcb2d540378
Jbritoloaiza22 <britoloaizajd33@hotmail.com>
2026-04-14 21:47:32 -0600

    doxygen in cortex files

--- STM32CubeIDE/src/Board/HAL/cortex.c
+++ STM32CubeIDE/src/Board/HAL/cortex.c
@@ -1,33 +1,129 @@
+/**
+ * @file cortex.c
+ * @brief Cortex-M NVIC and SysTick abstraction layer.
+ *
+ * This module provides a wrapper interface for configuring and controlling
+ * Cortex-M core features such as:
+ * - NVIC interrupt priority
+ * - Interrupt enable/disable
+ * - System reset
+ * - SysTick timer configuration
+ *
+ * It abstracts direct CMSIS calls to maintain portability and consistency
+ * within the kernel architecture.
+ *
+ * Responsibilities:
+ * - Encapsulate NVIC configuration
+ * - Provide a clean API for interrupt management
+ * - Configure system tick timer
+ *
+ * @note
+ * These functions are thin wrappers over CMSIS core functions.
+ *
+ * @warning
+ * Incorrect priority configuration may lead to interrupt starvation
+ * or unexpected system behavior.
+ *
+ * @author
+ * Jesus Daniel Britoloaiza
+ *
+ * @copyright
+ * Copyright (c) 2026 Jesus Daniel Britoloaiza
+ *
+ * @license
+ * This source code is provided for educational and research purposes.
+ */
+
 #include "stm32g0xx_hal.h"
 
-void vCORTEX_NVICSetPriority(IRQn_Type IRQn, uint32_t PreemptPriority, uint32_t SubPriority)
+/* =========================================================
+ * NVIC CONFIGURATION
+ * ========================================================= */
+
+/**
+ * @brief Set interrupt priority.
+ *
+ * Configures the priority of a given interrupt in the NVIC.
+ *
+ * @param[in] IRQn Interrupt number (IRQn_Type)
+ * @param[in] PreemptPriority Preemption priority level
+ * @param[in] SubPriority Subpriority level (not used in Cortex-M0+)
+ *
+ * @note
+ * On Cortex-M0+, only preemption priority is supported.
+ * SubPriority parameter is ignored.
+ */
+void vCORTEX_NVICSetPriority(IRQn_Type IRQn,
+                            uint32_t PreemptPriority,
+                            uint32_t SubPriority)
 {
-  NVIC_SetPriority(IRQn, PreemptPriority);
+    (void)SubPriority; /* Not used in Cortex-M0+ */
+    NVIC_SetPriority(IRQn, PreemptPriority);
 }
 
-
+/**
+ * @brief Enable interrupt in NVIC.
+ *
+ * Allows the specified interrupt to be serviced.
+ *
+ * @param[in] IRQn Interrupt number
+ */
 void vCORTEX_NVICEnableIRQ(IRQn_Type IRQn)
 {
-
-  NVIC_EnableIRQ(IRQn);
+    NVIC_EnableIRQ(IRQn);
 }
 
-
+/**
+ * @brief Disable interrupt in NVIC.
+ *
+ * Prevents the specified interrupt from being serviced.
+ *
+ * @param[in] IRQn Interrupt number
+ */
 void vCORTEX_NVICDisableIRQ(IRQn_Type IRQn)
 {
-  NVIC_DisableIRQ(IRQn);
+    NVIC_DisableIRQ(IRQn);
 }
 
+/* =========================================================
+ * SYSTEM CONTROL
+ * ========================================================= */
 
+/**
+ * @brief Perform a system reset.
+ *
+ * Triggers a software reset of the microcontroller.
+ *
+ * @note
+ * This function does not return.
+ */
 void vCORTEX_NVICSystemReset(void)
 {
-  /* System Reset */
-  NVIC_SystemReset();
+    NVIC_SystemReset();
 }
 
+/* =========================================================
+ * SYSTICK CONFIGURATION
+ * ========================================================= */
 
+/**
+ * @brief Configure SysTick timer.
+ *
+ * Initializes the SysTick timer to generate periodic interrupts.
+ *
+ * @param[in] TicksNumb Number of clock ticks between interrupts
+ *
+ * @return
+ * - 0: Success
+ * - 1: Failure (reload value out of range)
+ *
+ * @note
+ * Typically used to generate a 1 ms system tick:
+ * @code
+ * vCORTEX_SYSTICKConfig(SystemCoreClock / 1000U);
+ * @endcode
+ */
 uint32_t vCORTEX_SYSTICKConfig(uint32_t TicksNumb)
 {
-  return SysTick_Config(TicksNumb);
+    return SysTick_Config(TicksNumb);
 }
-
--- STM32CubeIDE/src/Board/HAL/cortex.h
+++ STM32CubeIDE/src/Board/HAL/cortex.h
@@ -1,5 +1,38 @@
-#ifndef STM32G0xx_HAL_CORTEX_H
-#define STM32G0xx_HAL_CORTEX_H
+/**
+ * @file cortex.h
+ * @brief Cortex-M NVIC and SysTick abstraction interface.
+ *
+ * This module defines the public API for configuring and controlling
+ * Cortex-M core features such as interrupt management and system timing.
+ *
+ * It acts as a lightweight abstraction layer over CMSIS functions,
+ * allowing better portability and integration within the system kernel.
+ *
+ * Features:
+ * - NVIC interrupt priority configuration
+ * - Interrupt enable/disable control
+ * - System reset control
+ * - SysTick timer configuration
+ *
+ * @note
+ * This layer wraps CMSIS core functions (NVIC, SysTick).
+ *
+ * @warning
+ * Incorrect usage of interrupt priorities may lead to unpredictable
+ * system behavior or priority inversion issues.
+ *
+ * @author
+ * Jesus Daniel Britoloaiza
+ *
+ * @copyright
+ * Copyright (c) 2026 Jesus Daniel Britoloaiza
+ *
+ * @license
+ * This source code is provided for educational and research purposes.
+ */
+
+#ifndef CORTEX_H
+#define CORTEX_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -7,16 +40,89 @@ extern "C" {
 
 #include "stm32g0xx_hal_def.h"
 
+/* =========================================================
+ * SYSTICK CLOCK SOURCE DEFINITIONS
+ * ========================================================= */
+
+/**
+ * @brief SysTick clock source: HCLK divided by 8.
+ */
 #define SYSTICK_CLKSOURCE_HCLK_DIV8       0x00000000U
+
+/**
+ * @brief SysTick clock source: HCLK.
+ */
 #define SYSTICK_CLKSOURCE_HCLK            0x00000004U
 
-void vCORTEX_NVICSetPriority(IRQn_Type IRQn, uint32_t PreemptPriority, uint32_t SubPriority);
+/* =========================================================
+ * PUBLIC API
+ * ========================================================= */
+
+/**
+ * @brief Set interrupt priority.
+ *
+ * Configures the priority level of the specified interrupt.
+ *
+ * @param[in] IRQn Interrupt number
+ * @param[in] PreemptPriority Preemption priority level
+ * @param[in] SubPriority Subpriority level (not used in Cortex-M0+)
+ *
+ * @note
+ * Cortex-M0+ only supports preemption priority.
+ * SubPriority parameter is ignored.
+ */
+void vCORTEX_NVICSetPriority(IRQn_Type IRQn,
+                            uint32_t PreemptPriority,
+                            uint32_t SubPriority);
+
+/**
+ * @brief Enable interrupt.
+ *
+ * Enables the specified interrupt in the NVIC.
+ *
+ * @param[in] IRQn Interrupt number
+ */
 void vCORTEX_NVICEnableIRQ(IRQn_Type IRQn);
+
+/**
+ * @brief Disable interrupt.
+ *
+ * Disables the specified interrupt in the NVIC.
+ *
+ * @param[in] IRQn Interrupt number
+ */
 void vCORTEX_NVICDisableIRQ(IRQn_Type IRQn);
+
+/**
+ * @brief Perform system reset.
+ *
+ * Triggers a software reset of the microcontroller.
+ *
+ * @note This function does not return.
+ */
 void vCORTEX_NVICSystemReset(void);
+
+/**
+ * @brief Configure SysTick timer.
+ *
+ * Initializes the SysTick timer to generate periodic interrupts.
+ *
+ * @param[in] TicksNumb Number of clock ticks between interrupts
+ *
+ * @return
+ * - 0: Success
+ * - 1: Failure (reload value out of range)
+ *
+ * @note
+ * Typical configuration for 1 ms tick:
+ * @code
+ * vCORTEX_SYSTICKConfig(SystemCoreClock / 1000U);
+ * @endcode
+ */
 uint32_t vCORTEX_SYSTICKConfig(uint32_t TicksNumb);
 
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* CORTEX_H */
-
-
-
